e782155e40b1ff21e7ce8c13cf2ac1e1c4da1345
sohail.sarfraz <sohail.sarfraz@confiz.com>
2021-08-17 23:54:38 +0500

    forecast file change

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (location, latitude, longitude, callback) => {
             callback('Unable to find location', undefined)
         } else {
             console.log('body',body);
-            callback(undefined, body.current.weather_descriptions[0] + ' It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.precip + '% chance of rain.')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress out. It feels like ' + body.current.feelslike + ' degree out. The humidity is ' + body.current.humidity + '. There is a ' + body.current.precip + '% chance of rain.')
         }
     })
 }
